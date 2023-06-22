3a589aa7bc2ada28fb8327d068574fb74328faf9
cuongnm <cuongnm01.paraline@gmail.com>
2023-06-22 15:36:45 +0700

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,11 +15,11 @@
         </div>
         <div class="navbar-nav w-100">
             @if(\Illuminate\Support\Facades\Auth::guard('admin')->check())
-            <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-            <a href="{{ route('management.account') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Account</a>
-            <a href="{{ route('management.report') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Report</a>
-            <a href="{{ route('management.customer') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Customer</a>
-            <a href="{{ route('management.report.import') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Import Excel</a>
+            <a href="{{ route('management.home') }}" class="nav-item nav-link {{ request()->is('management') ? 'active' : '' }}"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+            <a href="{{ route('management.account') }}" class="nav-item nav-link {{ request()->is('management/account*') ? 'active' : '' }}"><i class="fa fa-chart-bar me-2"></i>Account</a>
+            <a href="{{ route('management.report') }}" class="nav-item nav-link {{ (request()->is('management/report*') && !request()->is('management/report/import')) ? 'active' : '' }}"><i class="fa fa-chart-bar me-2"></i>Report</a>
+            <a href="{{ route('management.customer') }}" class="nav-item nav-link {{ request()->is('management/customer*') ? 'active' : '' }}"><i class="fa fa-chart-bar me-2"></i>Customer</a>
+            <a href="{{ route('management.report.import') }}" class="nav-item nav-link {{ request()->is('management/report/import') ? 'active' : '' }}"><i class="fa fa-chart-bar me-2"></i>Import Excel</a>
             @endif
             @if(\Illuminate\Support\Facades\Auth::guard('customer')->check())
                 <a href="{{ route('customer.account') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Customer</a>
