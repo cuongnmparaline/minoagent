96e5abcf0892ed312fdda5fcc053fb59734415f4
cuongboyhc <cuongboyhc@gmail.com>
2023-08-09 19:58:54 +0700

    09/08/2023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="">
                         <img src="./images/white-logo.svg" alt="Logo" />
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNine"
