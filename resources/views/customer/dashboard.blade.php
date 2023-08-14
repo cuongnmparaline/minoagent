8b3582d6a3621f2bb7f35f01f61e807ccca112da
cuongboyhc <cuongboyhc@gmail.com>
2023-08-14 23:02:46 +0700

    14/08/2023

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -37,7 +37,7 @@
             </div>
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                    <i class="fa fa-arrow-left fa-3x text-primary"></i>
                     <div class="ms-3">
                         <p class="mb-2">Comback</p>
                         <h6 class="mb-0">0</h6>
